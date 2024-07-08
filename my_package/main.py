af6cec44e0deabb90b3b96a866f8c1402c3da1c9
Dima <aldmikon@gmail.com>
2024-07-09 06:54:10 +0800

    List variables memory check

--- my_package/main.py
+++ my_package/main.py
@@ -1,11 +1,17 @@
-def is_palindrome(word='dad'):
-    
-    """Writing a function that checks if the word/phrase is a palindrome"""
-    
-    return word == word[::-1]
+import sys
 
+def check_allocated_memory(*args:list):
+    """Check how much space takes a variable of type list 
+    depending on the type of its elements"""
+    
+    for arg in args:
+        print(sys.getsizeof(arg))
+
+    return 
 
 if __name__ == "__main__":
-    print(is_palindrome())
-    #specific argument
-    print(is_palindrome('banana'))
+    a = [1,2,3,4,5,6]
+    b = ['1','2','3','4','5','6']
+    c = ['cat','dog','pig','g','fdfddfd','dffff']
+    check_allocated_memory(a,b,c)
+
