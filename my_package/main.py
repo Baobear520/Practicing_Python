9d4b10de3115b53fc2a58eeb6d4d95f33304b72f
Dima <aldmikon@gmail.com>
2024-07-09 13:48:29 +0800

    Playing with print(set)

--- my_package/main.py
+++ my_package/main.py
@@ -5,8 +5,22 @@ def remove_duplicates_and_sort(my_array:list):
     my_set = set(my_array)
     print(sorted(my_set))
 
+
+def check_set_of_ints(my_set_of_ints:set):
+    print(my_set_of_ints)
+
+def check_set_of_strs(my_set_of_strs:set):
+    print(my_set_of_strs)
+
 if __name__ == "__main__":
-    my_array = [3,2,6,1,7,4,2,6,9,3,5,5,7]
+    my_set_of_ints = {3,2,1,2,3}
+    check_set_of_ints(my_set_of_ints)
+
+    my_set_of_strs = {'b','d','a','c','b','a'}
+    check_set_of_strs(my_set_of_strs)
+    
+    my_array = [3,2,1,2,3]
     remove_duplicates_and_sort(my_array)
+
 
 
