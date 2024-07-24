06d3cbc3446414e2bcd3aeeb82265bb0bd01eddc
Dima <aldmikon@gmail.com>
2024-07-25 07:52:47 +0800

    Running asynchronous tasks consequently

--- my_package/practice.py
+++ my_package/practice.py
@@ -2,19 +2,22 @@ import asyncio
 from random import randint
 
 async def hello():
-    await asyncio.sleep(1)
+    await asyncio.sleep(3)
     print("Hello",end=" ")
 
 async def world():
+
     for char in "World":
-        await asyncio.sleep(randint(3,4))
+        await asyncio.sleep(randint(1,2))
         print(char,end="",flush=True)
 
 async def do_smth_else():
     print(sum(range(5)),end=" ")
     
 async def main():
-    await asyncio.gather(hello(), world(),do_smth_else())
+    await hello()
+    await world()
+    await do_smth_else()
     
 if __name__ == '__main__':
     asyncio.run(main())
