dde3153fb91293c4a51a1fe1b2dc59d2062099b4
Dima <aldmikon@gmail.com>
2024-07-28 09:35:19 +0800

    Passing arbitrary args to a function and unpacking

--- my_package/main.py
+++ my_package/main.py
@@ -1,12 +1,17 @@
-import copy
+def func(x,*args,y):
+    print(x)
+    
+    #if we call func(x,my_list,y)
+    print(args) # returns packed tuple ([1,2,3],)
+    print(*args) #unpacks the tuple of args and returns [1,2,3]
+    
+    #if we call func(x,*my_list,y) aka unpack the tuple of args:
+    #print(args) returns packed tuple (1,2,3)
+    #print(*args) returns a sequence 1 2 3
 
-a = 'dog'
-b = copy.copy(a)
 
-print(id(a),id(b))
+my_list1 = [1,2,3]
+my_list2 = [4,5,6]
 
-a += 's'
-print(id(a),id(b))
-
-b = copy.deepcopy(a)
-print(id(a),id(b))
+func(3,my_list1,y=1)
+func(3,*my_list1,y=1)
