7966581ff3c1b53751653b7a49353989dbf11724
Dima <aldmikon@gmail.com>
2024-08-06 07:04:20 +0800

    Reassignment of list elements during iteration

--- my_package/main.py
+++ my_package/main.py
@@ -1,20 +1,3 @@
-def data_source():
-    for i in range(10):
-        yield i
-
-def filter_even_numbers(data):
-    for item in data:
-        if item % 2 == 0:
-            yield item
-
-def square_numbers(data):
-    for item in data:
-        yield item * item
-
-# Usage
-pipeline = data_source()
-pipeline = filter_even_numbers(pipeline)
-pipeline = square_numbers(pipeline)
-
-for result in pipeline:
-    print(result)
+a = [0,1,2,3]
+for a[-1] in a: #reassigning a[-1] to the value of the current item in iteration
+    print(a[-1])
