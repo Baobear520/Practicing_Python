7e48beb93f817f4e2313de9b9d201f49936308d3
Dima <aldmikon@gmail.com>
2024-11-03 14:23:17 +0800

    Practicing unpacking args and kwargs

---
+++ my_package/unpacking_args_kwargs.py
@@ -0,0 +1,9 @@
+
+
+a, *b, c = [1,2]
+print(a,b,c)
+
+#The unpacking works as follows:
+# a is assigned the first element of the list, which is 1.
+# *b means that b will take all the remaining elements between a and c.
+# c is assigned the last element of the list, which is 2.
